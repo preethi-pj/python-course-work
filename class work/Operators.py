f1d4e22bc73da8a4c2cadf7c0acc609ca61af057
preethi-pj <cpreethijenifar@gmail.com>
2025-11-19 10:43:02 +0530

    operators

---
+++ class work/Operators.py
@@ -0,0 +1,122 @@
+'''
+PYTHON OPERATORS
+----------------
+'''
+
+#1.ARITHMETIC OPERATORS
+a=20
+b=10
+print("a+b:",a+b) #a+b:30
+print("a-b:",a-b) #a-b:10
+print("a*b:",a*b) #a*b:200
+print("a/b:",a/b) #a/b:2
+print("a//b:",a//b) #a//b:2
+print("a%b:",a%b) #a%b:0
+print("a**b:",a**b) #a**b:10240000000000
+
+#COMPARISION OPERATORS
+c=30
+d=40
+print('c<d:',c<d)  #c<d:True
+print('c>d:',c>d)  #c>d:False
+print('c<=d:',c<=d) #c<=d:True
+print('c>=d:',c>=d) #c>=d:False
+print('c==d:',c==d) #c==d:False
+print('c!=d:',c!=d) #c!=d:True
+
+#ASSIGNMENT OPERATORS
+'''
+var=var op val
+var op=val
+'''
+e=100
+e+=100
+print('e+=100:',e)  #e+=100:200
+e-=100
+print('e-=100:',e)  #e-=100:100
+e*=100
+print('e*=100:',e)  #e*=100:10000
+e//=100
+print('e//=100:',e) #e//=100:100
+e**=10
+print('e**=10:',e) #e**=10:100000000000000000000
+e%=10
+print('e%=10:',e)  #e%=10:1
+e/=5
+print('e/=5:',e)   #e/=5:0.2
+
+#LOGICAL OPERATORS
+x=100
+y=50
+print("x%20==0 and y%20==0:",x%20==0 and y%20==0) #x%20==0 and y%20==0 False
+print("x%20==0 or y%20==0:",x%20==0 or y%20==0)
+print("not x%20==0 and y%20==0:", not x%20==0)
+print("not y%20==0:",not y%20==0)
+
+
+#MEMBERSHIP OPERATORS(in,not in)
+s='python programming'
+print("'i' in s:",'i' in s)
+print("'x' not in s:",'x' not in s)
+
+l=[1,2,3,4,5]
+print("3 in l:",3 in l)
+print("10 not in l:",10 not in l)
+
+t=(102,103,104,105)
+print("108 in t:",108 in t)
+print("104 not in t:",104 not in t)
+
+s={10,20,30,40}
+print("40 in s:",40 in s)
+print("50 not in s:",50 not in s)
+
+d={1:1,2:4,3:9,4:16,5:25}
+print('1 in d:',1 in d)
+print('4 not in d:',4 not in d)
+
+#IDENTITY OPERATORS(need to share same memory location,is ,is not)
+a=[1,2,3,4]
+b=[1,2,3,4]
+print('a is b:',a is b)#a is b: False
+c=a
+print('a is c:',a is c)#a is c: True
+print('id(a):',id(a))#id(a):1485506026368
+print('id(b):',id(b))#id(b):1485507587328
+print('id(c):',id(c))#id(c):1485506026368
+
+print('a is not b:',a is not b)#a is not b:True
+print('a is not c:',a is not c)#a is not c:False
+
+#BITWISE OPERATORS(&,|,.....)
+print('4&5:',4&5)
+print('11|12:',11|12)
+print('14^15:',14^15)
+print('~16:',~16)
+print('8<<1:',8<<1)
+print('16>>1:',16>>1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
