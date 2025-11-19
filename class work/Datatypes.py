09119eac02d98eff079821111083eb6f608d391b
preethi-pj <cpreethijenifar@gmail.com>
2025-11-19 10:36:03 +0530

    datatypes

--- class work/Datatypes.py
+++ class work/Datatypes.py
@@ -2,3 +2,32 @@ a=10
 b=-5
 print("datatype is:",type(a))
 print("datatype is:",type(b))
+a=20.5
+b=33.22
+print("datatype is:",type(a))
+print("datatype is:",type(b))
+x=1+3j
+print("datatype is:",type(x))
+company="Amazon"
+print(type(company))
+l=["1","2","3","4","5"]
+print(type(l))
+cart_items = ["Shoes", "T-shirt", "Headphones"]
+print(type(cart_items))
+product_dimensions = (12.5, 9.8, 2.2)
+print(type(product_dimensions))
+available_sizes = {"S", "M", "L", "XL"}
+print(type(available_sizes))
+frozen_tags = frozenset(["Sale", "Limited Edition","Trending"])
+print(type(frozen_tags))
+product_details = {
+"name": "Wireless Mouse",
+"brand": "Logitech",
+"price": 899.99,
+"rating": 4.5
+}
+print(type(product_details))
+is_logged_in = True
+print(type(is_logged_in))
+delivery_partner = None
+print(type(delivery_partner))
